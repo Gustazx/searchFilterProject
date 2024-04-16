ae274339621128e8da4e5a651d6cec75e8aa7bf5
gusta <gustavodelima841@gmail.com>
2024-04-15 21:32:05 -0300

    QueryClient

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import "react-native-gesture-handler";
 import React from "react";
 import { PaperProvider } from "react-native-paper";
 import { ThemeProvider } from "styled-components/native";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 import { AppRoutes } from "@src/routes/appRoutes";
 import { paperTheme, styledTheme } from "@src/common/theme";
@@ -11,8 +14,10 @@ export default function App() {
   return (
     <ThemeProvider theme={styledTheme}>
       <PaperProvider theme={paperTheme}>
-        <AppRoutes />
-        <StatusBar />
+        <QueryClientProvider client={queryClient}>
+          <StatusBar />
+          <AppRoutes />
+        </QueryClientProvider>
       </PaperProvider>
     </ThemeProvider>
   );
